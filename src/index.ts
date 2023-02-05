752935a1e86688112501630462bab7c1f58c43b5
Brian Mulinge <61237677+Brianmulinge@users.noreply.github.com>
2023-02-04 20:20:14 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -47,7 +47,105 @@ enum Vehicletype {
   van
 }
 const type = Vehicletype.Lorry;
+const typeName = Vehicletype[type];
+
+//-All enum declarations with te same name inside a commonroot will contribute toward a single type
+//when defining an enum across multiple same blocks,subsequent blocks after the first declaration must specify the numeric value to be used to continue the sequence
+enum Boxsize {
+  small,
+  medium
+}
+enum Boxsize {
+  large = 2,
+  xlarge,
+  xxlarge
+}
+
+//Bit FLags
+/*
+-they allow a series of items to be selected or deselected by switching individual bits in a sequence on and off.
+-to ensure each value in an enumaration relates to a single bit, the numbering must follow the binary sequence whereby each value is a power of two
+-by defaults when you create a variable to store state, all items are switched off
+-to switch on an option, it can be simply assigned to the variable.
+-to switch on multiple items, items can be combined with the OR Operator
+*/
+
+//using flags
+enum DiscFlags {
+  None = 0,
+  Drive = 1,
+  Influence = 2,
+  Steadiness = 4,
+  Conscientiousness = 8
+}
+
+// Using flags
+var personality = DiscFlags.Drive | DiscFlags.Conscientiousness;
+
+//testing flags
+// true
+var hasD = (personality & DiscFlags.Drive) == DiscFlags.Drive;
+
+//Constant Enums
+//-can be created using const keyword
+//-they can be erased during compilation and all code referring to it is replaced with hard coded values
+const enum Vehicletypes {
+  pedalcycle,
+  motorcycle,
+  car,
+  Sedan,
+  Suv
+}
+
+const types = Vehicletypes.Suv;
+//to make the inlining of values possible, const enum are not allowed to have computed members
 
 //Union types
+//-widens the allowable values specifying that the value can be of more than a single type
+// Type annotation for a union type
+let union: boolean | number;
+// OK: number
+union = 5;
+// OK: boolean
+union = true;
+// Error: Type "string" is not assignable to type 'number | boolean'
+//union = 'string';
+// Type alias for a union type
+//type StringOrError = string | Error;
+// Type alias for union of many types
+//type SeriesOfTypes = string | number | boolean | Error;
 
-console.log(Name, Boy, age, Price, department, names, sayHello, type);
+//Literal Types
+//-used to narrow the rangle allowanble values to a subset of the type such as reducing a string to set of specific values
+
+type Kingdom =
+  | "Bacteria"
+  | "Protozoa"
+  | "Chromista"
+  | "Plantae"
+  | "Fungi"
+  | "Animalia";
+let kingdom: Kingdom;
+// OK
+kingdom = "Bacteria";
+//Literal unions are really just union types made up of specific values
+//you can also create a number literal type or a nuion/literal hybrid type using the same syntax
+
+//Output
+console.log(
+  Name,
+  Boy,
+  age,
+  Price,
+  department,
+  names,
+  sayHello,
+  type,
+  typeName,
+  Boxsize,
+  personality,
+  hasD,
+  types,
+  union,
+  kingdom
+);
